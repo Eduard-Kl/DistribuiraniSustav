533d8464c998ff1345e1d23547dad849a521cad5
mdomink <markoo.dominkovic@gmail.com>
2018-06-24 01:20:58 +0200

    Mala dorada

--- Projekt/Prodavaonica.java
+++ Projekt/Prodavaonica.java
@@ -1,16 +1,23 @@
 import java.util.concurrent.TimeUnit;
+import java.util.Scanner;
+import java.util.*;
 public class Prodavaonica extends Process implements Election {   
     String p="";
-    int next;   
+    Dictionary<String, Integer> proizvodi;
+    int next;
+    String name;   
     boolean awake = false;
-    public Prodavaonica(Linker initComm) {
+    public Prodavaonica(Linker initComm,Dictionary<String, Integer> proi) {
         super(initComm);
         next = (myId + 1) % N;
+	this.name="Prodavaonica "+String.valueOf(myId);
+	this.proizvodi=proi;
     }
     public synchronized String nadiProizvod(){
 	   while (p.equals("")) myWait();
 	   return p;
     }
+		
     /*public synchronized void handleMsg(Msg m, int src, String tag) {
         int j = m.getMessageInt(); // get the number
         if (tag.equals("election")) {
@@ -26,20 +33,34 @@ public class Prodavaonica extends Process implements Election {
         }
     }*/
 
+	public String parsiraj(String str){
+		String pomocni=str.split("\\r?\\n")[0];
+		//pomocni.substring(0, pomocni.indexOf(' '));
+		System.out.println("Parsirano: "+pomocni);
+		return pomocni;
+	}
+
 	public synchronized void handleMsg(Msg m, int src, String tag) {
-		String str = m.getMessage(); 
+		String poruka = m.getMessage().substring(1); // problem parisranja...doraditi
+		String poruka_proizvod=parsiraj(poruka);
 		if (tag.equals(String.valueOf(myId))) {
-			System.out.println("Poruka se vratila: "+ str);		
-		     	p=str;
+			System.out.println("Poruka se vratila: "+poruka);		
+		     	p=poruka;
 			notify();
 		} 
 		else {	
+			System.out.println("Trazi proizvod: "+poruka_proizvod);
+			if(proizvodi.get(poruka_proizvod)!=null && proizvodi.get(poruka_proizvod)>0){
+				System.out.println("Nađen: "+poruka);	
+				poruka=poruka+'\n'+name+" "+proizvodi.get(poruka_proizvod);
+			}
 			notify();		
-		   	sendMsg(next, tag, str);				
+		   	sendMsg(next, tag, poruka);				
 		}
 	}
 
-    public synchronized void startElection(String proizvod) {   
+    public synchronized void startElection(String proizvod) { 
+	p="";  
         sendMsg(next, String.valueOf(myId), proizvod);
     }
 }
--- Projekt/ProdavaonicaTester.java
+++ Projekt/ProdavaonicaTester.java
@@ -8,13 +8,13 @@ public class ProdavaonicaTester {
 		// Lista proizvoda
 		Dictionary<String, Integer> proizvodi = new Hashtable<String, Integer>();
 		proizvodi.put("cokolada", 5);
-		proizvodi.put("pivo", 7);
+		proizvodi.put("pivo", 2);
 		
 		int myId = Integer.parseInt(args[1]);
 		int numProc = Integer.parseInt(args[2]);
 		System.out.println("prvi "+args[0]+" myId: " + args[1]+ ", numProc: "+args[2]);
 		Linker comm = new Linker(args[0], myId, numProc);
-		Prodavaonica p = new Prodavaonica(comm);
+		Prodavaonica p = new Prodavaonica(comm,proizvodi);
 		for (int i = 0; i < numProc; i++){
 			if (i != myId);
 				(new ListenerThread(i, p)).start();
@@ -26,7 +26,7 @@ public class ProdavaonicaTester {
 			System.out.println("Proizvod: " + trazeniProizvod);
 
 			// Sadrži li dućan proizvod
-			if(proizvodi.get(trazeniProizvod) != null){
+			if(proizvodi.get(trazeniProizvod) != null && proizvodi.get(trazeniProizvod)>0){
 				System.out.println("Traženi proizvod dostupan je na skladištu, broj artikala: " + proizvodi.get(trazeniProizvod) + ".");
 				System.out.println("Prodaja izvršena.");
 				proizvodi.put(trazeniProizvod, proizvodi.get(trazeniProizvod) - 1);

5c6b9cb55b30cd254c22c88d0f39ed50cefe003e
mdomink <markoo.dominkovic@gmail.com>
2018-06-24 18:36:02 +0200

    Kupi i ubaci

--- Projekt/Prodavaonica.java
+++ Projekt/Prodavaonica.java
@@ -1,23 +1,24 @@
 import java.util.concurrent.TimeUnit;
 import java.util.Scanner;
 import java.util.*;
-public class Prodavaonica extends Process implements Election {   
+public class Prodavaonica extends Process implements Election {
     String p="";
     Dictionary<String, Integer> proizvodi;
     int next;
-    String name;   
+    String name;
+    Scanner reader = new Scanner(System.in);
     boolean awake = false;
     public Prodavaonica(Linker initComm,Dictionary<String, Integer> proi) {
         super(initComm);
         next = (myId + 1) % N;
-	this.name="Prodavaonica "+String.valueOf(myId);
+	this.name="Prodavaonica"+String.valueOf(myId);
 	this.proizvodi=proi;
     }
     public synchronized String nadiProizvod(){
 	   while (p.equals("")) myWait();
 	   return p;
     }
-		
+
     /*public synchronized void handleMsg(Msg m, int src, String tag) {
         int j = m.getMessageInt(); // get the number
         if (tag.equals("election")) {
@@ -34,7 +35,7 @@ public class Prodavaonica extends Process implements Election {
     }*/
 
 	public String parsiraj(String str){
-		String pomocni=str.split("\\r?\\n")[0];
+		String pomocni=str.split("\\?")[0];
 		//pomocni.substring(0, pomocni.indexOf(' '));
 		System.out.println("Parsirano: "+pomocni);
 		return pomocni;
@@ -44,23 +45,72 @@ public class Prodavaonica extends Process implements Election {
 		String poruka = m.getMessage().substring(1); // problem parisranja...doraditi
 		String poruka_proizvod=parsiraj(poruka);
 		if (tag.equals(String.valueOf(myId))) {
-			System.out.println("Poruka se vratila: "+poruka);		
+			System.out.println("Poruka se vratila: "+poruka);
 		     	p=poruka;
 			notify();
-		} 
-		else {	
+		}
+		else {
 			System.out.println("Trazi proizvod: "+poruka_proizvod);
 			if(proizvodi.get(poruka_proizvod)!=null && proizvodi.get(poruka_proizvod)>0){
-				System.out.println("Nađen: "+poruka);	
-				poruka=poruka+'\n'+name+" "+proizvodi.get(poruka_proizvod);
+				System.out.println("Nađen: "+poruka_proizvod);
+				poruka=poruka+name+" "+proizvodi.get(poruka_proizvod)+"?";
 			}
-			notify();		
-		   	sendMsg(next, tag, poruka);				
+			notify();
+		   	sendMsg(next, tag, poruka);
 		}
 	}
 
-    public synchronized void startElection(String proizvod) { 
-	p="";  
+  public synchronized void odradiKupnju(){  
+    String proizvod="";
+    System.out.print("Odaberi proizvod: ");
+    while(proizvod.equals(""))
+      proizvod = reader.nextLine();
+    if(proizvodi.get(proizvod) != null){
+      int brojPreostalih=proizvodi.get(proizvod);
+      System.out.print("Kolicina: ");
+      int kolicina = reader.nextInt();
+      if(kolicina<=brojPreostalih && kolicina>0){
+        System.out.println("Prodano: "+proizvod+" "+kolicina);
+        proizvodi.put(proizvod, proizvodi.get(proizvod) - kolicina);
+        return;
+      }
+      else{
+        System.out.println("Nema dovoljno kolicine");
+        return;
+      }
+    }
+    else
+      return;
+  }
+
+  public synchronized void dodajArtikl(){
+    String proizvod="";
+    System.out.print("Naziv artikla: ");
+    while(proizvod.equals(""))
+      proizvod = reader.nextLine();
+    System.out.print("Kolicina: ");
+    int kolicina= reader.nextInt();
+    proizvodi.put(proizvod, kolicina);
+  }
+
+	public synchronized void ostaleProdavaonice(String upit){ //trazenje prozvoda
+		String rezultat="";
+		startElection(upit+"?");
+		rezultat = nadiProizvod();
+		String[] popis=rezultat.split("\\?");
+		for(String s : popis)  //nacin ispisa proizvoda iz ostalih prodavaonica
+			System.out.println(s);
+
+	}
+
+  public int kolicinaProizvoda(String proizvod){ //vraca kolicinu prozivoda , 0 ako nema proizvoda
+    if(proizvodi.get(proizvod)==null)
+      return 0;
+    return proizvodi.get(proizvod);
+  }
+
+    public synchronized void startElection(String proizvod) {
+	p="";
         sendMsg(next, String.valueOf(myId), proizvod);
     }
 }
--- Projekt/ProdavaonicaTester.java
+++ Projekt/ProdavaonicaTester.java
@@ -4,12 +4,12 @@ import java.util.*;
 public class ProdavaonicaTester {
 
 	public static void main(String[] args) throws Exception{
-		
+
 		// Lista proizvoda
 		Dictionary<String, Integer> proizvodi = new Hashtable<String, Integer>();
 		proizvodi.put("cokolada", 5);
 		proizvodi.put("pivo", 2);
-		
+
 		int myId = Integer.parseInt(args[1]);
 		int numProc = Integer.parseInt(args[2]);
 		System.out.println("prvi "+args[0]+" myId: " + args[1]+ ", numProc: "+args[2]);
@@ -19,23 +19,26 @@ public class ProdavaonicaTester {
 			if (i != myId);
 				(new ListenerThread(i, p)).start();
 		}
-		String leader="";
-		while(true){ //bekonacna petlja za upite
+		String rezultat="";
+		while(true){ //beskonacna petlja za upite
+			System.out.println("Izbornik (Kupi/Ubaci)");
 			Scanner reader = new Scanner(System.in);
+			System.out.print("");
 			String trazeniProizvod = reader.nextLine();
-			System.out.println("Proizvod: " + trazeniProizvod);
+			//System.out.println("Proizvod: " + trazeniProizvod);
 
-			// Sadrži li dućan proizvod
-			if(proizvodi.get(trazeniProizvod) != null && proizvodi.get(trazeniProizvod)>0){
-				System.out.println("Traženi proizvod dostupan je na skladištu, broj artikala: " + proizvodi.get(trazeniProizvod) + ".");
-				System.out.println("Prodaja izvršena.");
-				proizvodi.put(trazeniProizvod, proizvodi.get(trazeniProizvod) - 1);
+			if(trazeniProizvod.equals("Kupi")){ //kupovina proizvoda
+				p.odradiKupnju();
 			}
-		 		
+			else if(trazeniProizvod.equals("Ubaci"))//dodavanje proizvoda
+				p.dodajArtikl();
+			// Sadrži li dućan proizvod
+			else if(p.kolicinaProizvoda(trazeniProizvod)>0){
+				System.out.println("Traženi proizvod dostupan je na skladištu, broj artikala: " + p.kolicinaProizvoda(trazeniProizvod) + ".");
+			}
+
 			else{ //ako nema trazi dalje
-				p.startElection(trazeniProizvod);
-				leader = p.nadiProizvod();
-				System.out.println("Proizvodi: " + leader);
+				p.ostaleProdavaonice(trazeniProizvod);
 			}
 		}
 
